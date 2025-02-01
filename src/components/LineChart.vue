4c85ca8acb079d102f669c98ecb538c39608f583
Zach Barbre <zach.barbre@gmail.com>
2025-02-01 09:39:50 -0500

    initial commit with vue setup, removal of some defaults and beginning on the line chart

---
+++ src/components/LineChart.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+  data: {
+    type: Array,
+    required: true,
+  },
+  width: {
+    type: Number,
+    default: 400,
+  },
+  height: {
+    type: Number,
+    default: 400,
+  },
+  margin: {
+    type: Object,
+    default: () => ({
+      top: 20,
+      right: 20,
+      bottom: 30,
+      left: 40,
+    }),
+  },
+})
+
+console.log('🚀 ~ props:', props.data)
+</script>
+
+<template>
+  <svg :width="width" :height="height"></svg>
+</template>
